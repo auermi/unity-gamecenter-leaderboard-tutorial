15be0d7be11ba4ff6a967f53d1d1e24ab91619b2
auermi <michael@michael-auer.co>
2015-07-02 14:29:33 -0400

    Commited base project

---
+++ Assets/Scripts/GameCenterTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SocialPlatforms;
+
+public class GameCenterTest : MonoBehaviour {
+	
+	public Transform scoreText;
+	public Transform checkScoreboardButton;
+	public Transform sendScoreButton;
+	public Transform slider;
+	
+	private long sliderVal;
+	
+	void Awake () {
+		// Get our slider value 
+		sliderVal = (long) slider.GetComponent<Slider>().value;
+		// Update Score Text
+		updateScoreText();
+		// Event listeners
+		slider.GetComponent<Slider>().onValueChanged.AddListener (delegate { updateScoreText(); });
+		checkScoreboardButton.GetComponent<Button>().onClick.AddListener(() => {  });
+		sendScoreButton.GetComponent<Button>().onClick.AddListener(() => {  });
+	}
+	
+	// We run this when the slider value is changed
+	void updateScoreText() {
+		sliderVal = (long) slider.GetComponent<Slider>().value;
+		scoreText.GetComponent<Text>().text = sliderVal.ToString();
+	} 
+	
+	
+}
